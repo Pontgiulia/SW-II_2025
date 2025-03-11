74a20fc16e14953d528630abda6f6a8408a43b82
Pontgiulia <pontelligiulia@gmail.com>
2025-03-11 08:33:51 -0300

    08

--- Aula03/08.php
+++ Aula03/08.php
@@ -1,3 +1,14 @@
 <?php
+    function aleatorio(){
+        $vetor = [];
+        for ($v=0; $v <10; $v++){
+            $vetor[$v] = rand();
+        }
 
+        return $vetor;
+    }
+
+    $recebeVetor = aleatorio();
+
+    print_r($recebeVetor)
 ?>
